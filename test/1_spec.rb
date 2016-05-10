05365b8cec6ffe4217c51dc0425db0d68da4d4b1
Annie Hedgpeth <annie.hedgpeth@gmail.com>
2016-05-10 12:31:33 -0500

    1.2.2 completed

--- test/1_spec.rb
+++ test/1_spec.rb
@@ -1,9 +1,17 @@
-control "cis-1-2-1" do                      # A unique ID for this control
-  impact 1.0                                # The criticality, if this control fails.
-  title "1.2.1 Verify CentOS GPG Key is Installed (Scored)"# A human-readable title
+control "cis-1-2-1" do                      
+  impact 1.0                                
+  title "1.2.1 Verify CentOS GPG Key is Installed (Scored)"
   desc "CentOS cryptographically signs updates with a GPG key to verify that they are valid."
   describe command('rpm -q --queryformat "%{SUMMARY}\n" gpg-pubkey') do
    its('stdout') { should match /CentOS 6 Official Signing Key/}
   end
 end
 
+control "cis-1-2-2" do
+  impact 1.0
+  title "1.2.2 Verify that gpgcheck is Globally Activated (Scored)"
+  desc "The gpgcheck option, found in the main section of the /etc/yum.conf file determines if an RPM package's signature is always checked prior to its installation."
+  describe file('/etc/yum.conf') do
+    its('content') { should match /gpgcheck=1/ }
+  end
+end
