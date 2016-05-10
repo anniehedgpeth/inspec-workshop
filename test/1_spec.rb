aca5ace9393f84e89fe4e80bc161287961eb63ae
Annie Hedgpeth <annie.hedgpeth@gmail.com>
2016-05-10 12:54:39 -0500

    1.5.1 simplified by using owner matcher

--- test/1_spec.rb
+++ test/1_spec.rb
@@ -20,7 +20,7 @@ control "cis-1-5-1" do
   impact 1.0
   title "1.5.1 Set User/Group Owner on /etc/grub.conf (Scored)"
   desc "Set the owner and group of /etc/grub.conf to the root user."
-  describe command('stat -L -c "%u %g" /etc/grub.conf | egrep "0 0"') do
-   its('stdout') { should match /0 0/}
+  describe file('/etc/grub.conf') do
+    its('owner') { should eq 'root' }
   end
 end
