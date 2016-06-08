2059f2875a81489f15d89827387ffb7dc557505d
Annie Hedgpeth <annie.hedgpeth@gmail.com>
2016-06-07 19:13:28 -0500

    4.2.5 passed

--- controls/4.rb
+++ controls/4.rb
@@ -66,16 +66,16 @@ control "cis-4-2-4" do
  its('value') { should eq 1 }
  end
 end
-#
-#control "cis-4-2-5" do
-#  impact 1.0
-#  title "4.2.5 Enable Ignore Broadcast Requests (Scored)"
-#  desc "Accepting ICMP echo and timestamp requests with broadcast or multicast destinations for #your network could be used to trick your host into starting (or participating) in a Smurf attack. #A Smurf attack relies on an attacker sending large amounts of ICMP broadcast messages with a #spoofed source address. All hosts receiving this message and responding would send echo-reply #messages back to the spoofed address, which is probably not routable. If many hosts respond to #the packets, the amount of traffic on the network could be significantly multiplied." 
-#  describe kernel_parameter('net.ipv4.icmp_echo_ignore_broadcasts') do
-#    its('value') { should eq 1 }
-#  end
-#end
-#
+
+control "cis-4-2-5" do
+ impact 1.0
+ title "4.2.5 Enable Ignore Broadcast Requests (Scored)"
+ desc "Accepting ICMP echo and timestamp requests with broadcast or multicast destinations for #your network could be used to trick your host into starting (or participating) in a Smurf attack. #A Smurf attack relies on an attacker sending large amounts of ICMP broadcast messages with a #spoofed source address. All hosts receiving this message and responding would send echo-reply #messages back to the spoofed address, which is probably not routable. If many hosts respond to #the packets, the amount of traffic on the network could be significantly multiplied." 
+ describe kernel_parameter('net.ipv4.icmp_echo_ignore_broadcasts') do
+   its('value') { should eq 1 }
+ end
+end
+
 control "cis-4-5-1" do
   impact 1.0
   title "4.5.1 Install TCP Wrappers (Not Scored)"
