33fbcce1ea4d64ae9902a7065157eab8be13be05
Annie Hedgpeth <annie.hedgpeth@gmail.com>
2016-06-08 23:41:24 -0500

    6.1.7 passed

--- controls/6.rb
+++ controls/6.rb
@@ -37,22 +37,15 @@ control "cis-6-1-6" do
   end
 end
 
-# control "cis-6-1-7" do
-#   impact 1.0
-#   title "6.1.7 Set User/Group Owner and Permission on /etc/cron.weekly (Scored)"
-#   desc "The /etc/cron.weekly directory contains system cron jobs that need to run on a weekly basis. The files in this directory cannot be manipulated by the crontab command, but are instead edited by system administrators using a text editor. The commands below restrict read/write and search access to user and group root, preventing regular users from accessing this directory." 
-#   describe file('/etc/cron.weekly') do
-#     it { should exist }
-#     it { should_not be_executable.by "group" }
-#     it { should_not be_readable.by "group" }
-#     it { should_not be_writable.by "group" }
-#     it { should_not be_executable.by "other" }
-#     it { should_not be_readable.by "other" }
-#     it { should_not be_writable.by "other" }
-#     its("gid") { should cmp 0 }
-#     its("uid") { should cmp 0 }
-#   end
-# end
+control "cis-6-1-7" do
+  impact 1.0
+  title "6.1.7 Set User/Group Owner and Permission on /etc/cron.weekly (Scored)"
+  desc "The /etc/cron.weekly directory contains system cron jobs that need to run on a weekly basis. The files in this directory cannot be manipulated by the crontab command, but are instead edited by system administrators using a text editor. The commands below restrict read/write and search access to user and group root, preventing regular users from accessing this directory." 
+  describe file('/etc/cron.weekly') do
+    it { should exist }
+    its('mode') {should eq 0600}
+  end
+end
 
 control "cis-6-1-8" do
   impact 1.0
