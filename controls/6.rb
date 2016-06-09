34156899f510c768ca2280bc092ff28c24c638fc
Annie Hedgpeth <annie.hedgpeth@gmail.com>
2016-06-08 19:43:00 -0500

    6.1.5 passed

--- controls/6.rb
+++ controls/6.rb
@@ -15,3 +15,12 @@ control "cis-6-1-4" do
     its('owner') { should eq 'root' }
   end
 end
+
+control "cis-6-1-5" do
+  impact 1.0
+  title "6.1.5 Set User/Group Owner and Permission on /etc/cron.hourly (Scored)"
+  desc "This directory contains system cron jobs that need to run on an hourly basis. The files in this directory cannot be manipulated by the crontab command, but are instead edited by system administrators using a text editor. The commands below restrict read/write and search access to user and group root, preventing regular users from accessing this directory." 
+  describe file('/etc/cron.hourly') do
+    its('owner') { should eq 'root' }
+  end
+end
