d3642ab5b44365d644f22b4f9bc54c396040988c
Annie Hedgpeth <annie.hedgpeth@gmail.com>
2016-06-08 19:40:47 -0500

    6.1.4 passed

--- controls/6.rb
+++ controls/6.rb
@@ -6,3 +6,12 @@ control "cis-6-1-2" do
     it { should be_enabled }
   end
 end
+
+control "cis-6-1-4" do
+  impact 1.0
+  title "6.1.4 Set User/Group Owner and Permission on /etc/crontab (Scored)"
+  desc "The /etc/crontab file is used by cron to control its own jobs. The commands in this item make here sure that root is the user and group owner of the file and is the only user that can read and write the file." 
+  describe file('/etc/crontab') do
+    its('owner') { should eq 'root' }
+  end
+end
